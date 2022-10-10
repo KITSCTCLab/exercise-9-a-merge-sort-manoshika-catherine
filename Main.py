c2306f39a893473406e647798d06dfe6b6fe1f80
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-10-10 22:09:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,33 @@
 from typing import List
 
 def merge_sort(myList) -> None:
-  if len(myList) > 1
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
 
         # Two iterators for traversing the two halves
-        i = 1
-        j = 1
+        i = 0
+        j = 0
         
         # Iterator for the main list
         k = 0
         
         while i < len(left) and j < len(right):
-           
+            if left[i] <= right[j]:
               # The value from the left half has been used
-     
-              i += 0
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
             else:
                 myList[k] = right[j]
                 j += 1
             # Move to the next slot
-            
+            k += 1
 
         # For all the remaining values
         while i < len(left):
@@ -28,7 +36,7 @@ def merge_sort(myList) -> None:
             k += 1
 
         while j < len(right):
-            myList[k]=right[j];;
+            myList[k]=right[j]
             j += 1
             k += 1
 
@@ -38,6 +46,8 @@ input_data = input()
 data = []
 for item in input_data.split(', '):
   if item.isnumeric():
-    data.append(int
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
 merge_sort(data)
 print(data)
